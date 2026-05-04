fcb5c51d0127f7fdcc9868866890ebcf4f02aba5
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2026-05-04 23:20:27 +0800

    feat: add error module with typed errors

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum SageTraceError {
+    #[error("log filter state lock poisoned")]
+    LockPoisoned,
+
+    #[error("invalid filter directive: {0}")]
+    InvalidDirective(#[from] tracing_subscriber::filter::ParseError),
+
+    #[error("failed to reload filter: {0}")]
+    Reload(#[from] tracing_subscriber::reload::Error),
+
+    #[error("I/O error: {0}")]
+    Io(#[from] io::Error),
+
+    #[error("failed to set global tracing subscriber: {0}")]
+    SetGlobalDefault(#[from] tracing::subscriber::SetGlobalDefaultError),
+}
+
+pub type Result<T> = std::result::Result<T, SageTraceError>;
